b0bfee1a39ada8061faa6050f7196efe6786350f
袁振 <31892yuanzhen@goldwind.com.cn>
2022-04-07 15:52:04 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  methods: {
+    deepClone(target) {
+      if(typeof(target) !== `object`) return target
+      let result
+      if(Object.prototype.toString.call(target) === `[object Array]`) {
+        result = []
+      } else {
+        result = {}
+      }
+      for (const key in target) {
+        if (target.hasOwnProperty(key)) {
+          result[key] = this.deepClone(target[key])
+        }
+      }
+      return result
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
